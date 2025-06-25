99f2b2131ca0f32d7ea537c6a639f01dc53667cb
Bima Putra Lamanda <biput98@gmail.com>
2025-06-25 22:21:43 +0700

    modify dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_PATH': JSON.stringify(process.env.VITE_API_PATH || 'http://localhost:3000/api')
-  },
   server: {
     host: '0.0.0.0',
     port: 5173,
