9e8e9c256918d5adf314647d2cbdd4673e6d8a67
Bima Putra Lamanda <biput98@gmail.com>
2025-06-26 10:40:30 +0700

    modify dockerfile

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,5 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['bimadev.com'],
-    proxy: {
-      '/api': {
-        target: 'http://209.38.95.60:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   }
 })
